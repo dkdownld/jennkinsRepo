67317c09fff3dc33e0851c14398a3fb41ae93225
dkdownld <dkdownld@gmail.com>
2020-02-21 15:20:25 +0530

    Added one more line to main messages

--- simple-maven-java/src/main/java/com/dk/HelloMain.java
+++ simple-maven-java/src/main/java/com/dk/HelloMain.java
@@ -5,6 +5,7 @@ public class HelloMain {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven");
 		System.out.println("working on jenkins as well with projectName/pom.xml");
+		System.out.println("Will I get this output as well");
 
 	}
 
