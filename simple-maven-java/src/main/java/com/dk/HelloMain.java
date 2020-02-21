bfef40825b81ff261c50a653bca84ddf068f6e8e
dkdownld <dkdownld@gmail.com>
2020-02-21 15:07:35 +0530

    Added to main message

--- simple-maven-java/src/main/java/com/dk/HelloMain.java
+++ simple-maven-java/src/main/java/com/dk/HelloMain.java
@@ -4,6 +4,7 @@ public class HelloMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Maven");
+		System.out.println("working on jenkins as well with projectName/pom.xml");
 
 	}
 
